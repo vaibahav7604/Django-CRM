dc085f6e6e47ed0f640faf5cfb161c6f24c0276e
Meghani Vaibhav <vaibhavmeghani79@gmail.com>
2025-03-18 16:22:25 +0530

    registation

--- dcrm/mydb.py
+++ dcrm/mydb.py
@@ -2,7 +2,28 @@ import mysql.connector
 # pip install mysql-connector-python
 # pip install mysql-connector
 # pip install mysql,pip insatall django ,django-admin startproject dcrm,
-# git commands:-
+''' git commands:-# Configuration commands
+git config --global user.name "Meghani Vaibhav"
+git config --global user.email "vaibhavmeghani79@gmail.com"
+git config --global push.default matching
+git config --global alias.co checkout
+
+# Initialize a new repository
+git init
+
+# Staging and committing changes
+git add .
+git commit -am 'initial commit'
+
+# Adding a remote repository
+git remote add origin https://github.com/vaibahav7604/Django-CRM
+
+# Rename the branch to main
+git branch -M main
+
+# Pushing changes to the remote repository
+git push -u origin main'''
+
 dataBase = mysql.connector.connect(
     host='localhost',
     user='root',
